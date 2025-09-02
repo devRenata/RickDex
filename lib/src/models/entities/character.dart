8412288069f270978f5c17d1df2a6b6c8d7eeaed
Renata Rocha <renata.rocha.polachini@gmail.com>
2025-09-02 19:34:04 -0300

    feat: modelado entidade Character

---
+++ lib/src/models/entities/character.dart
@@ -0,0 +1,61 @@
+class Character {
+  final int id;
+  final String name;
+  final String status;
+  final String species;
+  final String type;
+  final String gender;
+  final String origin;
+  final String location;
+  final String image;
+  final DateTime created;
+  final List<int> episodes;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    required this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.created,
+    required this.episodes,
+  });
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return Character(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      species: json['species'],
+      type: json['type'],
+      gender: json['gender'],
+      origin: json['origin']['name'],
+      location: json['location']['name'],
+      image: json['image'],
+      created: DateTime.parse(json['created']),
+      episodes: (json['episodes'] as List)
+          .map((url) => int.parse(url.split('/').last))
+          .toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'status': status,
+      'species': species,
+      'type': type,
+      'gender': gender,
+      'origin': origin,
+      'location': location,
+      'image': image,
+      'created': created.toIso8601String(),
+      'episodes': episodes,
+    };
+  }
+}
